bb11755e1f3624c96f9bbf9548b19dacd484e641
Victor <victorherrerocazurro@gmail.com>
2016-10-19 07:31:31 +0200

    Segundo Ciclo TDD: Implementar la prueba unitaria del caso de prueba 2

--- src/test/java/com/ejemplo/tdd/calculadora/LimitesPrecisionUT.java
+++ src/test/java/com/ejemplo/tdd/calculadora/LimitesPrecisionUT.java
@@ -19,7 +19,7 @@ public class LimitesPrecisionUT {
 	 * Caso de prueba 1-Si el ingreso es de 0, el impuesto es 0
 	 */
 	@Test
-	public void limiteDePrecisionCorrecto() {
+	public void limiteDePrecisionCorrectoCasoDePrueba1() {
 		
 		//Datos de prueba
 		float ingreso = 0;
@@ -35,4 +35,28 @@ public class LimitesPrecisionUT {
 		Assert.assertEquals(impuestoEsperado, impuestoObtenido, 0.001);
 	}
 
+	/**
+	 * Caso de prueba 2-Si el ingreso es de 9.999.999.999.999.999, el impuesto es de 1.950.000.000.000.000
+	 */
+	
+	@Test
+	public void limiteDePrecisionCorrectoCasoDePrueba2() {
+
+		//Datos de prueba
+		float ingreso = 9_999_999_999_999_999f;
+		
+		//Con la precisión float, no se consigue la precision adecuada, 
+		//asi que se necesita que sea double
+		double impuestoEsperado = 1_950_000_000_000_000d;
+		
+		//Definicion de SUT
+		CalculadoraImpuestos sut = new CalculadoraImpuestos();
+		
+		//Ejecución del codigo
+		float impuestoObtenido = sut.calcularImpuestosDeIngresos(ingreso);
+		
+		//Validacion
+		Assert.assertEquals(impuestoEsperado, impuestoObtenido, 0.001);
+	}
+	
 }
