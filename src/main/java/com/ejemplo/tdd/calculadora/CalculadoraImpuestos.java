f31014de3d8dc1068db2ab1b4b2612db9ed3416e
Victor <victorherrerocazurro@gmail.com>
2016-10-19 07:17:32 +0200

    Primer Ciclo TDD: Poner a verde la prueba unitaria del caso de prueba 1

---
+++ src/main/java/com/ejemplo/tdd/calculadora/CalculadoraImpuestos.java
@@ -0,0 +1,10 @@
+package com.ejemplo.tdd.calculadora;
+
+public class CalculadoraImpuestos {
+
+	public float calcularImpuestosDeIngresos(float ingreso) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
